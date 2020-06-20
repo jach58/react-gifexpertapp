91736f699f76a899213f224ffffcfdda38923726
Juan Castillo <jach58@hotmail.com>
2020-06-20 17:14:19 -0500

    docs agregado

---
+++ docs/precache-manifest.cbf9790713f00ff39564d0cae7254732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca1da60bd186b2fcac5575b859d255e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202b69107e0c4e59a6c7",
+    "url": "/static/css/main.628c2ae6.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202b69107e0c4e59a6c7",
+    "url": "/static/js/main.70d0fdba.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
